848a6fc30517268fc8d751cec2aae8f7e3d18f3c
Fmt2022 <florisputs2001@gmail.com>
2025-02-12 12:46:30 +0100

    GateKeeperV3

---
+++ README.md
@@ -0,0 +1,6 @@
+install phyton <br>
+install pip <br>
+install pyinstaller <br>
+
+use command:
+python -m PyInstaller --onefile main.py
--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 from datetime import datetime
 
-def get_reaction():
-    now = datetime.now()
-    current_hour = int(now.strftime("%H"))
-    if current_hour >= 6 and current_hour <= 12:
-        return  "Goedemorgen"
-    elif current_hour >= 13 and current_hour <= 18:
-        return  "Goedemiddag"
-    elif current_hour >= 19 and current_hour <= 23:
-        return  "Goedenavond"
+i = 1
+while i == 1:
+    kenteken_input = input("Enter kenteken:")
+    kenteken_control = ["AB", "HS", "LR"]
+
+    def get_reaction():
+        now = datetime.now()
+        current_hour = int(now.strftime("%H"))
+        if current_hour >= 6 and current_hour <= 12:
+            return  "Goedemorgen"
+        elif current_hour >= 13 and current_hour <= 18:
+            return  "Goedemiddag"
+        elif current_hour >= 19 and current_hour <= 23:
+            return  "Goedenavond"
+        else:
+            return "Sorry, de parkeerplaats is ’s nachts gesloten"
+
+    if kenteken_input in kenteken_control:
+        print(get_reaction(), '! Welkom bij Fonteyn Vakantieparken')
     else:
-        return "Sorry, de parkeerplaats is ’s nachts gesloten"
-
-
-print(get_reaction(), '! Welkom bij Fonteyn Vakantieparken')
-
+        print("U heeft helaas geen toegang tot het parkeerterrein")
